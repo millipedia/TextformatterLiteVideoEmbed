6c1e1adf0b5db8df314315ad35bebf787b36dd61
millipedia <stephen@millipedia.co.uk>
2023-04-28 11:05:13 +0100

    Fixing path for Gideon.

--- TextformatterLiteVideoEmbed.js
+++ TextformatterLiteVideoEmbed.js
@@ -7,8 +7,8 @@ if (typeof window.text_formatter_lite_video_embed_scripts_loaded === 'undefined'
 	async function loadYTLite() {
 
 		//TODO set flags just load whichever one we need?
-		const LiteYTEmbed = await import('.' + tflve_script_path + '/lite-youtube.js');
-		const LiteVimeoEmbed = await import('.' + tflve_script_path + '/lite-vimeo.js');
+		const LiteYTEmbed = await import(tflve_script_path + '/lite-youtube.js');
+		const LiteVimeoEmbed = await import(tflve_script_path + '/lite-vimeo.js');
 		
 	}
 
